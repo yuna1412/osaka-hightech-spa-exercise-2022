4d88abe460cfcf9648a19e6fea335b003ccec0c1
yuna1412 <85538944+yuna1412@users.noreply.github.com>
2022-06-22 10:57:30 +0900

    saisin

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // TODO: 公式リファレンスに沿って JavaScript を記述
-const TwoWayBinding = {
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
-}
-Vue.createApp(TwoWayBinding).mount('#two-way-binding')
+const RootComponent = {};
+
+Vue.createApp(RootComponent).mount('#app');
